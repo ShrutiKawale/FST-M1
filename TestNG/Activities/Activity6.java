a26321539f709b10e71108ee75b8edeb431633de
Shruti Kawale <shruti.kawale@ibm.com>
2022-04-18 22:31:16 +0530

    adding testNG activities

---
+++ TestNG/Activities/Activity6.java
@@ -0,0 +1,40 @@
+package Activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Activity6 {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.get("https://www.training-support.net/selenium/login-form");
+    }
+
+    @Test
+    @Parameters({"username", "password"})
+    public void loginTestCase(String username, String password) {
+
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, admin");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
+}
